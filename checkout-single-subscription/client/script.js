89591d162464b69325ef2cf7eb18a163e709231f
frez <rezende.f@outlook.com>
2024-01-17 15:01:43 +0800

    testing new event handler for cancellations

--- checkout-single-subscription/client/script.js
+++ checkout-single-subscription/client/script.js
@@ -1,9 +1,29 @@
-/* Fetch prices and update the form */
+
+/* Fetch prices and update the form 
+
+This code will check if the elements exist 
+before trying to update their values
+
+index.html should contain the following elements: 
+comboPrice, silverPrice, goldPrice
+
+If any of these elements are missing, the server will 
+return a POST request with non-type value
+*/
+
+
+
 fetch("/config")
   .then(r => r.json())
-  .then(({basicPrice, proPrice}) => {
-    const basicPriceInput = document.querySelector('#basicPrice');
-    basicPriceInput.value = basicPrice;
-    const proPriceInput = document.querySelector('#proPrice');
-    proPriceInput.value = proPrice;
+  .then(({comboPrice, silverPrice, goldPrice, oneOff}) => {
+    const comboPriceInput = document.querySelector('#comboPrice');
+    comboPriceInput.value = comboPrice;
+    const silverPriceInput = document.querySelector('#silverPrice');
+    silverPriceInput.value = silverPrice;
+    const goldPriceInput = document.querySelector('#goldPrice');
+    goldPriceInput.value = goldPrice;
+    const oneOffInput = document.querySelector('#oneOff');
+    oneOffInput.value = oneOff;
   })
+  
+
