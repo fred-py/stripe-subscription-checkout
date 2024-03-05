0c5ddac7803626d251d5a93dbf29f8bcae78601c
frez <rezende.f@outlook.com>
2024-03-05 13:37:22 +0800

    auth view and templates have been updated. Users can now update and change both password and email.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,14 @@
-from flask import render_template, redirect, request, url_for, flash, current_app
-from flask_login import login_user, logout_user, login_required, \
-    current_user
+from flask import render_template, redirect, request, \
+    url_for, flash, current_app
+from flask_login import login_user, logout_user, \
+    login_required, current_user
 from . import auth
 from .. import db
 from app.models import User
 from app.emails import send_email
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, \
+    ChangePasswordForm, PasswordResetRequestForm, \
+    PasswordResetForm, ChangeEmailForm
 
 
 @auth.before_request  # Page. 286 before_request handler to intercept all blueprint requests
@@ -20,15 +23,18 @@ def before_request():     # @before_app_request can be used to intercept all req
     4. The request is not for a static file"""
     if current_user.is_authenticated \
             and not current_user.confirmed \
+            and request.endpoint \
             and request.blueprint != 'auth' \
             and request.endpoint != 'static':
         return redirect(url_for('auth.unconfirmed'))
+
 
 @auth.route('/unconfirmed')
 def unconfirmed():
     if current_user.is_anonymous or current_user.confirmed:
         return redirect(url_for('db_views.index'))
     return render_template('database/auth/unconfirmed.html')
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -95,4 +101,84 @@ def resend_confirmation():
     send_email(current_user.email, 'Confirm Your Account',
                'auth/email/confirm', user=current_user, token=token)
     flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('db_views.index'))
+
+
+# Password Updates
+@auth.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.old_password.data):
+            current_user.password = form.password.data
+            db.session.add(current_user)
+            db.session.commit()
+            flash('Your password has been updated.')
+            return redirect(url_for('main.index'))
+        else:
+            flash('Invalid password.')
+    return render_template("database/auth/change_password.html", form=form)
+
+
+@auth.route('/reset', methods=['GET', 'POST'])
+def password_reset_request():
+    if not current_user.is_anonymous:
+        return redirect(url_for('db_views.index'))
+    form = PasswordResetRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data.lower()).first()
+        if user:
+            token = user.generate_reset_token()
+            send_email(user.email, 'Reset Your Password',
+                       'database/auth/email/reset_password',
+                       user=user, token=token,
+                       next=request.args.get('next'))
+        flash('An email with instructions to reset your password has been sent to you.')
+        return redirect(url_for('auth.login'))
+    return render_template('database/auth/reset_password.html', form=form)
+
+
+@auth.route('/reset/<token>', methods=['GET', 'POST'])
+def password_reset(token):
+    if not current_user.is_anonymous:
+        return redirect(url_for('main.index'))
+    form = PasswordResetForm()
+    if form.validate_on_submit():
+        if User.reset_password(token, form.password.data):
+            db.session.commit()
+            flash('Your password has been updated.')
+            return redirect(url_for('auth.login'))
+        else:
+            return redirect(url_for('db_views.index'))
+    return render_template('database/auth/reset_password.html', form=form)
+
+
+@auth.route('/change_email', methods=['GET', 'POST'])
+@login_required
+def change_email_request():
+    form = ChangeEmailForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.password.data):
+            new_email = form.email.data.lower()
+            token = current_user.generate_email_change_token(new_email)
+            send_email(new_email, 'Confirm your email address',
+                       'database/auth/email/change_email',
+                       user=current_user, token=token)
+            flash('An email with instructions to confirm your new email '
+                  'address has been sent to you.')
+            return redirect(url_for('db_views.index'))
+        else:
+            flash('Invalid email or password.')
+    return render_template("database/auth/change_email.html", form=form)
+
+
+@auth.route('/change_email/<token>')
+@login_required
+def change_email(token):
+    if current_user.change_email(token):
+        db.session.commit()
+        flash('Your email address has been updated.')
+    else:
+        flash('Invalid request.')
     return redirect(url_for('db_views.index'))
