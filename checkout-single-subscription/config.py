4dde0b2fb65378d1814c6f2a8ed9d4c32a892a8d
frez <rezende.f@outlook.com>
2024-05-23 13:41:19 +0800

    Config class & children classes were wrapped inside create_config_classes func

--- checkout-single-subscription/config.py
+++ checkout-single-subscription/config.py
@@ -8,61 +8,65 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(find_dotenv('.env.dev.prod'))
 
 
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')  # flask-wtf 
-    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower() in \
-        ['true', 'on', '1']
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    UNITED_MAIL_SUBJECT_PREFIX = '[United]'
-    UNITED_MAIL_SENDER = 'United Admin <pythonapi2023@gmail.com>'
-    UNITED_ADMIN = os.getenv('UNITED_ADMIN')
-    UNITED_ADMIN_1 = os.getenv('UNITED_ADMIN_1')
-    UNITED_ADMIN_2 = os.getenv('UNITED_ADMIN_2')
-    UNITED_ADMIN_3 = os.getenv('UNITED_ADMIN_3')
-    UNITED_DRIVER = os.getenv('UNITED_DRIVER')
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #HOST = int(os.getenv('HOST', 4242))  # This is needed to deploy on fl0
-    
-    @staticmethod
-    def init_app(app):
-        pass
+def create_config_classes():
+
+    class Config:
+        SECRET_KEY = os.getenv('SECRET_KEY')  # flask-wtf 
+        MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.googlemail.com')
+        MAIL_PORT = int(os.getenv('MAIL_PORT', '587'))
+        MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower() in \
+            ['true', 'on', '1']
+        MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+        MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+        UNITED_MAIL_SUBJECT_PREFIX = '[United]'
+        UNITED_MAIL_SENDER = 'United Admin <pythonapi2023@gmail.com>'
+        UNITED_ADMIN = os.getenv('UNITED_ADMIN')
+        UNITED_ADMIN_1 = os.getenv('UNITED_ADMIN_1')
+        UNITED_ADMIN_2 = os.getenv('UNITED_ADMIN_2')
+        UNITED_ADMIN_3 = os.getenv('UNITED_ADMIN_3')
+        UNITED_DRIVER = os.getenv('UNITED_DRIVER')
+        MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+        #HOST = int(os.getenv('HOST', 4242))  # This is needed to deploy on fl0
+
+        @staticmethod
+        def init_app(app):
+            pass
 
 
-# Different configurations for the app to run in different environments
-class DevelopmentConfig(Config):
-    DEBUG = True
+    # Different configurations for the app to run in different environments
+    class DevelopmentConfig(Config):
+        DEBUG = True
 
-    #def __init__(self):
-    #    self.SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-
-
-class TestingConfig(Config):
-    TESTING = True
-    # Stripe Prices - TEST MODE
-
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL_TEST') or \
-        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+        #def __init__(self):
+        #    self.SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
-class ProductionConfig(Config):
-    DEBUG = False
-    # Assignment of the database URL to the SQLALCHEMY_DATABASE_URI
-    # This has been moved into the __init__ method
-    # Tis class definition was running before env variables were loaded
-    # Now in create_app() function, an instance of the Config class is created
+    class TestingConfig(Config):
+        TESTING = True
+        # Stripe Prices - TEST MODE
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL_TEST') or \
+            'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
-config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
+    class ProductionConfig(Config):
+        DEBUG = False
+        # Assignment of the database URL to the SQLALCHEMY_DATABASE_URI
+        # This has been moved into the __init__ method
+        # Tis class definition was running before env variables were loaded
+        # Now in create_app() function, an instance of the Config class is created
 
-    'default': DevelopmentConfig,
-}
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+    return {
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig,
+
+        'default': DevelopmentConfig,
+    }
+
+
+config = create_config_classes()
